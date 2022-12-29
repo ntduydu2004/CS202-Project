67983e601fcca2173a4a81581184a48fe5813661
LucidNg <94208867+LucidNg@users.noreply.github.com>
2022-12-30 00:25:52 +0700

    remodifed character

--- include/player.h
+++ include/player.h
@@ -1,32 +1,26 @@
 #pragma once
 #include <bits/stdc++.h>
 #include "raylib.h"
+#include "map.h"
 using namespace std;
 class Character{
 private:
+    vector <vector <Texture2D>> moveState;
+    string name;
+    Vector2 PositionChoose;
+public:
     short moveside; // 0: up, 1: right, 2: down, 3: left
     short movestate;
-    vector <vector <Texture2D>> moveState;
-    string characterName;
-public:
-    Character();
-    Character(string name);
-    ~Character();
-    const string& name();
-    void DrawInGame(Vector2 position);
-    void DrawChoose(Vector2 position);
-    void move(int direction);
-};
-
-class Player {
-private:
     Vector2 position;
-    string playerName;
-    Character *pCharacter;
-public:
-    void SetCharacter(Character *_pCharacter);
-    void SetNamePlayer(string playerName);
-    void setPosition(Vector2 _position);
-    void draw();
-    void move();
+    string namePlayer;
+    Character(string name, Vector2 PositionChoose);
+    void Follow(Map& GameMap, int IncreaseSpeed);
+    ~Character();
+    void Move();
+    void DrawInGame();
+    void DrawChoose();
+    void ChangeState(short& frames, Map& GameMap);
+    void CheckCollisionObject(Map& GameMap, bool& isCollided);
+    void Load(ifstream& fin);
+    void Save(ofstream& fout);
 };
