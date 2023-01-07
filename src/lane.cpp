d1dd0a23fff0fa8ec19c83f04eb09fa76c3ba042
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-01-07 22:00:53 +0700

    fix lỗi

--- src/lane.cpp
+++ src/lane.cpp
@@ -1,19 +1,6 @@
 #include "lane.h"
 using namespace std;
-Lane::Lane(string TypeOfLane, short height){
-    string Filename = "../data/image/Lane/" + TypeOfLane + ".png";
-    Pic = LoadTexture(Filename.c_str());
-    this->height = height;
-}
-Lane::~Lane(){
-    UnloadTexture(Pic);
-}
-void Lane::Draw(Vector2 position){
-    DrawTextureEx(Pic, position, 0.0f, 1.0f, WHITE);
-}
-short Lane::GetHeight(){
-    return height;
-}
+
 void LaneFactory::Draw(int type, Vector2 position){
     lane[type].Draw(position);
 }
