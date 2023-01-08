00190d36147f042ef26c52b1dd5ce6b338d2d93d
produy <nguyentrinhduy2003@gmail.com>
2023-01-08 22:49:13 +0700

    final fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,19 @@ int main(void)
     bool close = false;
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    
     Game game;
+    game.LoadScore();
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         game.run(close);
         // Draw
         //----------------------------------------------------------------------------------
-        // DrawEnterNamePhase(mousePosition, touchPosition, indexMouse, indexTouch, menu, rec_EnterName, background, characterName, id);
+        // DrawEnterNamePhase(mousePosition, touchPosition, indexMouse, indexTouch, menu, rec_EnterName, background, name, id);
         if (close) break;
         //----------------------------------------------------------------------------------
     }
-
-
+    game.SaveScore();
     CloseWindow();        // Close window and OpenGL context
     return 0;
 }
